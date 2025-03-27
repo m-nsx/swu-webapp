80b92471d5d9f92a39ef61a59cc0bbe2ddb901a6
Titouan <verdier.titouan@gmail.com>
2025-03-27 01:16:12 +0100

    added add/delete cards

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-export const connectDB = async () => {
+const connectDB = async () => {
+    const mongoServer = await MongoMemoryServer.create({
+        instance: {
+            dbPath: './backend/data', // Specify the path to store the database files
+            dbName: 'swudb', // Specify the database name
+        },
+    });
+
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected: " + conn.connection.host);
+        const uri = mongoServer.getUri();
+        const conn = await mongoose.connect(uri, {});
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+        // Vérifier et créer une collection pour s'assurer que la base de données est créée
+        const db = conn.connection.db;
+        const collections = await db.listCollections().toArray();
+        if (collections.length === 0) {
+            await db.createCollection('defaultCollection');
+            console.log('Default collection created to ensure database creation.');
+        }
     } catch (error) {
-        console.error('Error: ${error.message}');
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
+
+export default connectDB;
